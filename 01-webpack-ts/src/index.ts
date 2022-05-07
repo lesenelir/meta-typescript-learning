329efddd60fa312e2eacd503ee2b4a083c83f121
lesenelir <miaomiaobabyzy@gmail.com>
2022-05-07 21:24:31 +0800

    update

--- 01-webpack-ts/src/index.ts
+++ 01-webpack-ts/src/index.ts
@@ -1,5 +1,10 @@
+import {hello} from './test'
+
 function sum(a: number, b: number): number {
     return a + b
 }
 
 console.log(sum(1, 2))
+
+console.log("hello world + TS")
+console.log(hello)
--- 01-webpack-ts/webpack.config.js
+++ 01-webpack-ts/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
 
 // webpack 中的所有配置信息都写在module.exports
 module.exports = {
+  mode: "production",
   // 入口文件
   entry: "./src/index.ts",
   // 指定打包文件的目录
@@ -14,11 +18,47 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: [
+          // 配置babel 适配老浏览器
+          // {
+          //   loader: 'babel-loader',
+          //   options: {
+          //     // 设置预定义的环境
+          //     preset: [
+          //       [
+          //         // 执行环境的插件
+          //         "@babel/preset-env",
+          //         // 配置信息
+          //         {
+          //           targets: {
+          //             "chrome": "88",
+          //             "ie": "11"
+          //           },
+          //           "corejs": "3",
+          //           "useBuiltIns": "usage"
+          //         }
+          //       ]
+          //     ]
+          //   }
+          // },
+          'ts-loader'
+        ],
         // 要排除的文件模块
         exclude: /node_modules/
       }
     ]
+  },
+  // 配置插件
+  plugins: [
+    new HTMLWebpackPlugin({
+      // title: 'webpack-TS-Title',
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  // 用来设置引用模块
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 
 }
